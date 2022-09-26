7b8e5acad2a94fff4685c5e466dd64580c41f0a8
MJ-git-maker <jimei@ucsd.edu>
2022-09-26 14:58:23 -0700

    updated graph
    updated graph for m_6

--- rps-model-summary/_build/jupyter_execute/RLModel_code.py
+++ rps-model-summary/_build/jupyter_execute/RLModel_code.py
@@ -60,14 +60,14 @@ soft_dist = get_softmax_probabilities(
 df_a = pick_move(df_a, soft_dist)
 
 
-# In[30]:
+# In[7]:
 
 
 df_a=df_a[df_a['is_bot']==0]
 df_a=assign_agent_outcomes(df_a)
 
 
-# In[40]:
+# In[8]:
 
 
 plot_win_rates(win_summary(groupby_f_data(df_a,'agent_outcome',30),'agent_outcome'))
@@ -87,115 +87,85 @@ plot_win_rates(win_summary(groupby_f_data(df_a,'agent_outcome',30),'agent_outcom
 
 # ### 3b human_past_current_reward_move
 
-# In[12]:
+# In[10]:
 
 
-plot_win_rates(win_summary(groupby_f_data(df_result_3b,'agent_outcome',30),'agent_outcome'))
+separated = separate_df(df)
+for e in separated:
+    human_reward_past_cur_move(e)
+df_b = pd.concat(separated)
+
+
+# In[21]:
+
+
+separated = separate_df(df_b)
+df_result_b = pd.DataFrame()
+for e in separated:
+    e = get_softmax_probabilities_3b(e)
+    e=pick_move_3b(e)
+    e['agent_outcome'] = e.apply(lambda x: evaluate_outcome(x['agent_move'], x['opponent_move']), axis=1)
+    df_result_b=pd.concat([df_result_b,e],axis=0)
+
+
+# In[22]:
+
+
+plot_win_rates(win_summary(groupby_f_data(df_result_b,'agent_outcome',30),'agent_outcome'))
 
 
 # ### 3c opponent_past_human_current_reward_move
 
-# In[13]:
-
-
-def human_reward_oppo_past_cur_move(sub_df):
-    '''
-    tally rewards based on both opponent past and human current moves
-    '''
-    dic_reward={'win':3,'tie':0,'loss':-1}
-    dic_move={'rock':{'rock':0,'paper':0,'scissors':0},'paper':{'rock':0,'paper':0,'scissors':0},
-    'scissors':{'rock':0,'paper':0,'scissors':0}}
-    for i in range(2,len(sub_df),2):
-        outcome=sub_df.get('player_outcome').iloc[i]
-        oppo_pre_move=sub_df.get('player_move').iloc[i-1]
-        cur_move=sub_df.get('player_move').iloc[i]
-        if oppo_pre_move != 'none'and not pd.isna(oppo_pre_move) and cur_move != 'none'and not pd.isna(cur_move): # this line may skip nan row, reward may look like 0,0,0
-            dic_move[oppo_pre_move][cur_move]+=dic_reward[outcome]
-            col_name='opponent_'+oppo_pre_move+'_'+cur_move+'_reward'
-            sub_df.at[i,'opponent_rock_rock_reward']=dic_move['rock']['rock']
-            sub_df.at[i,'opponent_rock_paper_reward']=dic_move['rock']['paper']
-            sub_df.at[i,'opponent_rock_scissors_reward']=dic_move['rock']['scissors']
-            sub_df.at[i,'opponent_paper_rock_reward']=dic_move['paper']['rock']
-            sub_df.at[i,'opponent_paper_paper_reward']=dic_move['paper']['paper']
-            sub_df.at[i,'opponent_paper_scissors_reward']=dic_move['paper']['scissors']
-            sub_df.at[i,'opponent_scissors_rock_reward']=dic_move['scissors']['rock']
-            sub_df.at[i,'opponent_scissors_paper_reward']=dic_move['scissors']['paper']
-            sub_df.at[i,'opponent_scissors_scissors_reward']=dic_move['scissors']['scissors'] 
-    return sub_df 
-
-
-# In[14]:
+# In[27]:
 
 
 # separate df into same game id
 separated = separate_df(df)
 for e in separated:
     human_reward_oppo_past_cur_move(e)
-df = pd.concat(separated)
+df_c = pd.concat(separated)
 
 
-# In[15]:
+# In[25]:
 
 
-def get_softmax_probabilities_3c(df):
-    '''
-    generate softmax probability distribution of each round so we can sample moves from the distribution
-    '''
-    # df.dropna(axis = 0)
-    distribution = []
-    vals=[[0.33,0.33,0.33]] # has deleted one default prob list
-    for i in range(2,df.shape[0],2):
-        pre_move=df.get('player_move').iloc[i-1] # -1 instead of -2 since opponent_pre
-        if pre_move != 'none' and not pd.isna(pre_move):
-            reward_cols=[f'opponent_{pre_move}_rock_reward',f'opponent_{pre_move}_paper_reward',f'opponent_{pre_move}_scissors_reward']
-            val = df[reward_cols].iloc[i].tolist()
-            vals.append(val)
-        else:
-            val=vals[-1]
-            vals.append(val)
-    soft_max=[softmax(x) for x in vals] 
-    sofm = pd.DataFrame(soft_max, columns = ['softmax_prob_rock', 'softmax_prob_paper', 'softmax_prob_scissors'])
-    
-    # strip only human df outside of the function
-    df_new = df[df.is_bot == 0].reset_index()
-    df_new = pd.concat([df_new,sofm], axis = 1)
-    return df_new
-
-
-# In[16]:
-
-
-def pick_move_3c(df):
-    '''
-    sample agent move based on softmax distribution
-    '''
-    moves = np.array([])
-    for i in range(df.shape[0]):
-        move_choices = ['rock', 'paper', 'scissors']
-        distribution = df[['softmax_prob_rock', 'softmax_prob_paper', 'softmax_prob_scissors']].iloc[i].tolist() # get ith [rock_prob,paper_prob,scissors_prob] from input df 
-        chosen_move = random.choices(move_choices, distribution) 
-        moves = np.append(moves, chosen_move)
-    df = df.assign(agent_move = moves) # agent_move stores sampled moves
-    return df
-
-
-# In[17]:
-
-
-separated = separate_df(df)
-df_result_3c = pd.DataFrame()
+separated = separate_df(df_c)
+df_result_c = pd.DataFrame()
 # align results from the generaed agent move and opponent move
 for e in separated:
     e = get_softmax_probabilities_3c(e)
     e=pick_move_3c(e)
     e['agent_outcome'] = e.apply(lambda x: evaluate_outcome(x['agent_move'], x['opponent_move']), axis=1)
-    df_result_3c=pd.concat([df_result_3c,e],axis=0)
+    df_result_c=pd.concat([df_result_c,e],axis=0)
 
 
-# In[18]:
+# In[26]:
 
 
-plot_win_rates(win_summary(groupby_f_data(df_result_3c,'agent_outcome',30),'agent_outcome'))
+plot_win_rates(win_summary(groupby_f_data(df_result_c,'agent_outcome',30),'agent_outcome'))
+
+
+# ### 3d) opponent_past_human_past_current_move (mix)
+
+# In[29]:
+
+
+separated_agent_past = separate_df(df_b)
+separated_oppo_past=separate_df(df_c)
+df_result_mix = pd.DataFrame()
+count=0
+for i in range(len(separated_oppo_past)):
+# for e_agent,e_oppo in separated_agent_past, separated_oppo_past:
+    e=get_softmax_probabilities_mix(separated_agent_past[i], separated_oppo_past[i])
+    e=pick_move_3d(e)
+    e['agent_outcome'] = e.apply(lambda x: evaluate_outcome(x['agent_move'], x['opponent_move']), axis=1)
+    df_result_mix=pd.concat([df_result_mix,e],axis=0)
+
+
+# In[30]:
+
+
+plot_win_rates(win_summary(groupby_f_data(df_result_mix,'agent_outcome',30),'agent_outcome'))
 
 
 # In[ ]:
--- rps-model-summary/_build/jupyter_execute/RL_model_python_lib_decision_functions.py
+++ rps-model-summary/_build/jupyter_execute/RL_model_python_lib_decision_functions.py
@@ -11,6 +11,7 @@ Functions for converting counts to probabilities, evaluating agent decision and 
 """
 
 # from utils import *
+import random
 from random import choices
 
 
@@ -90,6 +91,43 @@ def get_softmax_probabilities_3c(df):
     df_new = pd.concat([df_new,sofm], axis = 1)
     return df_new
 
+def get_softmax_probabilities_mix(df_agent_past,df_opponent_past):
+    """
+    choose human_reward_past or opponent_reward_past based on which strategy has higher reward
+    """
+    
+    distribution = []
+    vals=[[0.33,0.33,0.33]] # has deleted one default prob list
+    for i in range(2,max(df_agent_past.shape[0],df_agent_past.shape[0]),2):
+        oppo_pre_move=df_agent_past.get('player_move').iloc[i-1]
+        agent_pre_move=df_agent_past.get('player_move').iloc[i-2]
+        
+        if agent_pre_move != 'none' and oppo_pre_move!= 'none' and not pd.isna(oppo_pre_move) and not pd.isna(agent_pre_move):
+            agent_reward_cols=[f'{agent_pre_move}_rock_reward',
+                         f'{agent_pre_move}_paper_reward',
+                         f'{agent_pre_move}_scissors_reward'] #df_agent_past only has bot 0
+            oppo_reward_cols=[f'opponent_{oppo_pre_move}_rock_reward',
+                         f'opponent_{oppo_pre_move}_paper_reward',
+                         f'opponent_{oppo_pre_move}_scissors_reward']# df_opponent_past has bot=0&1,so index not match
+            val_agent=df_agent_past[agent_reward_cols].iloc[i].tolist()
+            val_oppo = df_opponent_past[oppo_reward_cols].iloc[i].tolist()
+            if sum(val_agent)>sum(val_oppo):
+                val=val_agent
+            else:
+                val=val_oppo
+            vals.append(val)
+        else:
+            val=vals[-1]
+            vals.append(val)
+            
+    soft_max=[softmax(x) for x in vals] 
+    sofm = pd.DataFrame(soft_max, columns = ['softmax_prob_rock', 'softmax_prob_paper', 'softmax_prob_scissors'])
+    
+    # strip only human df outside of the function
+    df_new = df_agent_past[df_agent_past.is_bot == 0].reset_index() 
+    df_new = pd.concat([df_new,sofm], axis = 1)
+    return df_new
+
 #############################
 ### Move choice functions ###
 #############################
@@ -131,6 +169,16 @@ def pick_move_3c(df):
     df = df.assign(agent_move = moves) # agent_move stores sampled moves
     return df
 
+def pick_move_3d(df):
+    moves = np.array([])
+    for i in range(df.shape[0]):
+        move_choices = ['rock', 'paper', 'scissors']
+        distribution = df[['softmax_prob_rock', 'softmax_prob_paper', 'softmax_prob_scissors']].iloc[i].tolist() # get ith [rock_prob,paper_prob,scissors_prob] from input df 
+        chosen_move = random.choices(move_choices, distribution) 
+        moves = np.append(moves, chosen_move)
+    df = df.assign(agent_move = moves) # agent_move stores sampled moves
+    return df
+
 def assign_agent_outcomes(df):
     """
     Assign outcomes for the agent based on agent move choices.
@@ -139,4 +187,10 @@ def assign_agent_outcomes(df):
     df.assign(agent_outcome = '')
     df=df.assign(agent_outcome=df.apply(lambda x: evaluate_outcome(x['agent_move'], x['opponent_move']), axis=1))
     return df
+
+
+# In[ ]:
+
+
+
 
